3a2e6bc471bff5519435ca83f4d79a5360999210
Mirella <mirella.rivas@utec.edu.pe>
2022-06-22 12:40:04 -0500

    commit de boton de registro + rutas

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const router = express.Router();
 const db = require('./db');
 const usuarios = require('./routes/usuarios');
+const index = require('./routes/index');
 
 const path = __dirname + '/views/';
 const port = 8080;
@@ -12,6 +13,8 @@ app.set('view engine', 'html');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path));
 app.use('/usuarios', usuarios);
+// Anadido https://www.youtube.com/watch?v=bab8b2Ix4K0&ab_channel=Bluuweb
+app.use('/', index);
 
 app.listen(port, function () {
   console.log('Example app listening on port 8080!')
--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,29 @@ router.get('/',function(req,res){
   res.sendFile(path.resolve('views/index.html'));
 });
 
+// Todo lo anadido basado en https://www.youtube.com/watch?v=bab8b2Ix4K0&ab_channel=Bluuweb
+
+// Anadido : Inicio Sesion
+router.use (function (req,res,next) {
+  console.log('/iniciosesion' + req.method);
+  next();
+});
+
+
+router.get('/iniciosesion',function(req,res){
+  res.sendFile(path.resolve('views/IniciarSesion.html'));
+});
+
+// Anadido : Registro
+router.use (function (req,res,next) {
+  console.log('/registro' + req.method);
+  next();
+});
+
+
+router.get('/registro',function(req,res){
+  res.sendFile(path.resolve('views/Registrarse.html'));
+});
+
+
 module.exports = router;
